56c2de447fa8e6b8c402ee6574273c6c55a32537
Varun Kumar <varun.kumar@yale.edu>
2023-11-21 18:11:01 -0500

    first commit

---
+++ lib/Funnel.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Funnel/version"
+
+module Funnel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Funnel/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Funnel
+  VERSION = "0.1.0"
+end
